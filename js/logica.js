55a10fe3a498b0e25f1f20a10d5640ef67e74f97
Junior Loureiro <gelson.junior_@hotmail.com>
2024-08-19 16:38:04 -0300

    Atualizacao no header e pagina home

---
+++ js/logica.js
@@ -0,0 +1,19 @@
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(${(i - index) * 100}%)`;
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 6000); // Altera o slide a cada 6 segundos
+
+showSlide(currentSlide); // Inicializa o primeiro slide
